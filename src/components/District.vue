6fe44b19efea927b59fb085dc4d7d970d35d1a48
William Andree <williamandree@gmail.com>
2018-04-18 19:38:22 -0500

    added tabs on what is currently being called the district page. added button effects adding borders and other shhhh later.

--- src/components/District.vue
+++ src/components/District.vue
@@ -8,15 +8,14 @@
     </div>
     <!-- body -->
     <div class="hotBod">
-      <b-nav fill tabs v-bind:class="active" v-on:click.prevent>
-        <b-nav-item class="home" v-on:click="makeActive( generalInfo )">General</b-nav-item>
-        <b-nav-item class="projects" v-on:click="makeActive( eligibility )">Eligibility</b-nav-item>
-        <b-nav-item class="services" v-on:click="makeActive( IDRequirements )">ID Requirements</b-nav-item>
-        <b-nav-item class="contact" v-on:click="makeActive( voteTools )">Voter Tools</b-nav-item>
-      </b-nav>
+      <nav v-bind:class="active" v-on:click.prevent>
+        <a class="home tabItem" v-on:click="makeActive( generalInfo )"><button>General</button></a>
+        <a class="projects tabItem" v-on:click="makeActive( eligibility )"><button>Eligibility</button></a>
+        <a class="services tabItem" v-on:click="makeActive( IDRequirements )"><button>ID Requirements</button></a>
+        <a class="contact tabItem" v-on:click="makeActive( voteTools )"><button>Voter Tools</button></a>
+      </nav>
 
-      <p id="activeBod">
-        {{ active }}
+      <p id="activeBod" v-html="active">
       </p>
 
     </div>
@@ -47,10 +46,46 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-  b-nav-item:hover {
-    background-color: white;
-  }
-  .imBlue {
-    background-color: blue;
-  }
+
+#activeBod {
+  background-color: #fff;
+  border-top: 1px solid #716E10;
+  padding: 1rem;
+  min-height: 75vh;
+  text-align: left;
+}
+nav {
+  display: flex;
+}
+
+a {
+  border-top-right-radius: 2px;
+  border-top: 1px solid rgba(#716e10, 0.77);
+  border-left: 1px solid rgba(#716e10, 0.77);
+  border-right: 1px solid rgba(#716e10, 0.77);
+}
+button {
+  background-color: #f5f4ea;
+  border-top-left-radius: 2px;
+  padding: .5rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  margin-left: .5rem;
+  outline: none;
+  border: none;
+  transition: all 300ms cubic-bezier(0.645, 0.045, 0.355, 1);
+}
+button:hover {
+  background-color: #d5d755;
+}
+button:focus {
+  outline: 0;
+}
+.tabItem:link, .tabItem:visited {
+    cursor: auto;
+    text-decoration: none;
+}
+button:active {
+    background-color: #fff !important;
+}
 </style>
