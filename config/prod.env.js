aa53b84ca3c5e39eb05a9483c6623680fc20ea2e
William Andree <williamandree@gmail.com>
2018-06-27 20:57:58 -0500

    testing prod build

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,11 @@
-
+require('dotenv').config()
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  API_KEY: JSON.stringify(process.env.API_KEY),
+  GOOGLE_API_KEY: JSON.stringify(process.env.GOOGLE_API_KEY),
+  plugins: [
+    new Dotenv()
+  ]
 }
