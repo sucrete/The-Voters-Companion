e391da97e74bd051ba41a65df4621b52ee455b51
William Andree <williamandree@gmail.com>
2020-08-31 10:50:54 -0500

    saving data objects in code for unsupported version of the app

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # The Voter's Companion
 
- A guide to registering, getting to know your representatives, and when and where to vote. 🇺🇸
+A guide to registering, getting to know your representatives, and when and where to vote. 🇺🇸
 
- demo → https://thevoterscompanion.herokuapp.com/
+demo → https://thevoterscompanion.herokuapp.com/
+
+###
+
+`npm run dev` starts local server
--- dist/server.js
+++ dist/server.js
@@ -1,12 +1,12 @@
 // require('dotenv').config();
 // const Dotenv = require('dotenv-webpack');
 
-var express = require('express');
+var express = require("express");
 
-var serveStatic = require('serve-static');
-const bodyParser = require('body-parser');
-var rp = require('request-promise');
-var compression = require('compression');
+var serveStatic = require("serve-static");
+const bodyParser = require("body-parser");
+var rp = require("request-promise");
+var compression = require("compression");
 
 app = express();
 
@@ -14,36 +14,46 @@ app.use(compression());
 
 app.use(serveStatic(__dirname)); //middleware
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
 app.use(function(err, req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
-var VoterAPI = {karuthers: 'all that\'s fit to print',
-  stateIDs: 'bobby',
-  electionInfo: 'bobby again',
-  voterInfo: 'stinger'
+var VoterAPI = {
+  karuthers: "all that's fit to print",
+  stateIDs: "bobby",
+  electionInfo: "bobby again",
+  voterInfo: "stinger"
 };
 
 // GET INFORMATION FROM THE U.S. VOTE API
-app.post('/api/postVoterAPI', function(req, res, next) {
-  console.log('\n' + '\t' + '+----------- postVoterAPI started -----------+' + '\n');
+app.post("/api/postVoterAPI", function(req, res, next) {
+  console.log(
+    "\n" + "\t" + "+----------- postVoterAPI started -----------+" + "\n"
+  );
   const hardUP = req.body.data.USVoteKey;
   var stateName = req.body.data.voterStateName;
-  console.log('\n' + '\t' + '+----------- stateName = ' + stateName + ' -----------+' + '\n');
-  var stateID = '';
+  console.log(
+    "\n" +
+      "\t" +
+      "+----------- stateName = " +
+      stateName +
+      " -----------+" +
+      "\n"
+  );
+  var stateID = "";
   var statesOptions = {
-    uri: 'https://api.usvotefoundation.org/elections/v1/states',
-    method: 'GET',
+    uri: "https://api.usvotefoundation.org/elections/v1/states",
+    method: "GET",
     headers: {
-      'Authorization': 'Token ' + hardUP,
-      'Content-Type': 'application/json, text/plain, */*'
+      Authorization: "Token " + hardUP,
+      "Content-Type": "application/json, text/plain, */*"
     },
     qs: {
       limit: 56
@@ -53,20 +63,27 @@ app.post('/api/postVoterAPI', function(req, res, next) {
   rp(statesOptions)
     .then(response => {
       VoterAPI.stateIDs = response;
-      console.log('\n' + '\n' + 'working' + '\n' + '\n' + '...thank god' + '\n' + '\n');
+      console.log(
+        "\n" +
+          "\n" +
+          "futureElections" +
+          "\n" +
+          "\n" +
+          "...thank god" +
+          "\n" +
+          "\n"
+      );
       console.log(response);
       response.objects.forEach(where => {
         if (where.name === stateName) {
-          console.log(where.name);
-          stateID = where.id
+          stateID = where.id;
         }
       });
-      console.log('\n' + '\n' + '\t' + '+------- stateID = ' + stateID + ' -------+' + '\n')
       var electionsOptions = {
-        uri: 'https://api.usvotefoundation.org/elections/v1/elections',
+        uri: "https://api.usvotefoundation.org/elections/v1/elections",
         headers: {
-          'Authorization': 'Token ' + hardUP,
-          'Cache-Control': 'no-cache'
+          Authorization: "Token " + hardUP,
+          "Cache-Control": "no-cache"
         },
         qs: {
           state_id: stateID
@@ -76,12 +93,22 @@ app.post('/api/postVoterAPI', function(req, res, next) {
       rp(electionsOptions)
         .then(response => {
           VoterAPI.electionInfo = response;
-          console.log('\n' + '\n' + 'twerking' + '\n' + '\n' + '...thank god' + '\n' + '\n');
+          console.log(
+            "\n" +
+              "\n" +
+              "stateVoterInformation" +
+              "\n" +
+              "\n" +
+              "...thank god" +
+              "\n" +
+              "\n"
+          );
           console.log(response);
           var voterInfoOptions = {
-            uri: 'https://api.usvotefoundation.org/elections/v1/state_voter_information',
+            uri:
+              "https://api.usvotefoundation.org/elections/v1/state_voter_information",
             headers: {
-              'Authorization': 'Token ' + hardUP
+              Authorization: "Token " + hardUP
             },
             qs: {
               state_id: stateID
@@ -91,36 +118,52 @@ app.post('/api/postVoterAPI', function(req, res, next) {
           rp(voterInfoOptions)
             .then(response => {
               VoterAPI.voterInfo = response;
-              console.log('\n' + '\n' + 'borking' + '\n' + '\n' + '...thank god' + '\n' + '\n');
+              console.log(
+                "\n" +
+                  "\n" +
+                  "voterInfo" +
+                  "\n" +
+                  "\n" +
+                  "...thank god" +
+                  "\n" +
+                  "\n"
+              );
               console.log(response);
-              console.log('\n' + '\n' + 'U.S. Vote Info LOGGED to API');
-              res.status(200).end('the res.end() worked on postVoterAPI method on server');
+              console.log("\n" + "\n" + "U.S. Vote Info LOGGED to API");
+              res
+                .status(200)
+                .end("the res.end() worked on postVoterAPI method on server");
             })
             .catch(err => {
-              console.log('voterInfo call failed. error ----> ' + err)
+              console.log("voterInfo call failed. error ----> " + err);
               res.status(500).send({ error: err });
             });
         })
         .catch(err => {
-          console.log('electionInfo call failed. error ----> ' + err)
+          console.log("electionInfo call failed. error ----> " + err);
         });
     })
     .catch(err => {
-      console.log('stateIDs call failed. error ----> ' + err)
+      console.log("stateIDs call failed. error ----> " + err);
     });
 });
 
-app.get('/api/getVoterAPI', function(req, res) {
-  console.log('GETVOTERAPI method fired on server' + '\n' + '\n' + JSON.stringify(VoterAPI, null, '\t'));
+app.get("/api/getVoterAPI", function(req, res) {
+  console.log(
+    "GETVOTERAPI method fired on server" +
+      "\n" +
+      "\n" +
+      JSON.stringify(VoterAPI, null, "\t")
+  );
   // res.send({info: stateInfo});
   // make some calls to database, fetch some data, information, check state, etc...
   // convert whatever we want to send (preferably should be an object) to JSON
   res.json(VoterAPI);
-  res.status(200).end('the res.end() worked on getVoterAPI method on server');
+  res.status(200).end("the res.end() worked on getVoterAPI method on server");
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log('listening on localhost: 5000, papi')
+  console.log("listening on localhost: 5000, papi");
 });
-console.log('server started '+ port);
+console.log("server started " + port);
